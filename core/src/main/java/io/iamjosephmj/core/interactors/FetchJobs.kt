c51d80352a24ec6e889020d481ffe94bdef4f1ec
joseph james <iamjosephmj@gmail.com>
2021-03-17 01:35:45 +0530

    adding use case class

---
+++ core/src/main/java/io/iamjosephmj/core/interactors/FetchJobs.kt
@@ -0,0 +1,12 @@
+package io.iamjosephmj.core.interactors
+
+import io.iamjosephmj.core.data.models.GitHubJobDescription
+import io.iamjosephmj.core.data.models.SearchRequest
+import io.iamjosephmj.core.data.repo.GitHubJobsRepository
+import io.reactivex.rxjava3.core.Single
+
+class FetchJobs(private val gitHubJobsRepository: GitHubJobsRepository) {
+    operator fun invoke(searchRequest: SearchRequest): Single<List<GitHubJobDescription>> {
+        return gitHubJobsRepository.searchJobs(searchRequest)
+    }
+}
